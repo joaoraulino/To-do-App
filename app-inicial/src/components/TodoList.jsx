2e7b0e33a4eba550ed5fd3abb68f1a1c51e90abd
Joao Victor Raulino <116692477+joaoraulino@users.noreply.github.com>
2023-09-15 23:06:21 +0000

    atualização na estilização e funcionalidade do enter com a função do botão

--- app-inicial/src/components/TodoList.jsx
+++ app-inicial/src/components/TodoList.jsx
@@ -3,40 +3,46 @@ import React, { useState } from 'react';
 function TodoList() {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
-  const [filter, setFilter] = useState('all');
+  const [filter, setFilter] = useState('tudo');
 
   const handleInputChange = (event) => {
     setTask(event.target.value);
   };
 
-  const handleAddTask = () => {
+  const adicionarTarefa = () => {
     if (task.trim() !== '') {
-      setTasks([...tasks, { text: task, completed: false }]);
+      setTasks([...tasks, { text: task, completa: false }]);
       setTask('');
     }
   };
 
-  const handleDeleteTask = (index) => {
+  const handleInputKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      adicionarTarefa();
+    }
+  };
+
+  const concluida = (index) => {
     const updatedTasks = [...tasks];
     updatedTasks.splice(index, 1);
     setTasks(updatedTasks);
   };
 
-  const toggleTaskCompletion = (index) => {
+  const mudarConcluida = (index) => {
     const updatedTasks = [...tasks];
-    updatedTasks[index].completed = !updatedTasks[index].completed;
+    updatedTasks[index].completa = !updatedTasks[index].completa;
     setTasks(updatedTasks);
   };
 
-  const handleClearCompleted = () => {
-    const updatedTasks = tasks.filter(task => !task.completed);
+  const limparConcluida = () => {
+    const updatedTasks = tasks.filter(task => !task.completa);
     setTasks(updatedTasks);
   };
 
-  const filteredTasks = filter === 'completed'
-    ? tasks.filter(task => task.completed)
-    : filter === 'pending'
-      ? tasks.filter(task => !task.completed)
+  const filteredTasks = filter === 'completa'
+    ? tasks.filter(task => task.completa)
+    : filter === 'pendente'
+      ? tasks.filter(task => !task.completa)
       : tasks;
 
   const taskCount = tasks.length;
@@ -44,36 +50,42 @@ function TodoList() {
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
       <h1 className='font-bold text-3xl'>To-Do List</h1>
-      <div className='gap-2 flex flex-col flex-wrap h-[660px] w-[500px] min-w-400 justify-center border-black border-solid border-2 font-mono font-semibold text-base p-4'>
+      <div className='gap-2 flex flex-col flex-wrap h-[660px]  rounded-lg w-[500px] min-w-400 justify-center border-black border-solid border-2 font-mono font-semibold text-base p-4'>
           <div className=' grid gap-2'>
             <input className="border-black border-solid border-2 rounded-lg h-10 p-2" type="text" placeholder='Insira uma tarefa'
               value={task}
-              onChange={handleInputChange}
+              onChange={handleInputChange} 
+              onKeyPress={handleInputKeyPress}
             />
-            <button className="h-10 rounded-lg border-2 border-black border-solid" onClick={handleAddTask}>Adicionar</button>
+            <button className="h-10 rounded-lg border-2 border-black border-solid" onClick={adicionarTarefa}>Adicionar</button>
           </div>
-          <div className="h-[450px] w-[460px] border-black border-solid border-2">
-            <ul >
+          <div className="h-[450px] w-[460px] rounded-lg border-black border-solid border-2 p-3 max-h-400px overflow-y-auto">
+            <ul className="w-full max-w-[400px] overflow-hidden">
               {filteredTasks.map((task, index) => (
-                <li key={index}>
-                  {task.text}{' '}
-                  <button onClick={() => handleDeleteTask(index)}>x</button>{' '}
-                  <input
-                    type="checkbox"
-                    checked={task.completed}
-                    onChange={() => toggleTaskCompletion(index)}
-                  />
+                <li
+                  key={index}
+                  className="flex items-center justify-between max-w-100"
+                >
+                  <div style={{ maxWidth: "80%" }}>{task.text}</div>
+                  <div>
+                    <button onClick={() => concluida(index)}>X</button>{" "}
+                    <input
+                      type="checkbox"
+                      checked={task.completa}
+                      onChange={() => mudarConcluida(index)}
+                    />
+                  </div>
                 </li>
               ))}
             </ul>
           </div>
-
+          
           <div className="flex flex-row items-center justify-center gap-8">
             <p className=''>{taskCount === 1 ? '1 tarefa' : `${taskCount} tarefas`}</p>
-            <button onClick={() => setFilter('all')}>Todas</button>
-            <button onClick={() => setFilter('completed')}>Concluídas</button>
-            <button onClick={() => setFilter('pending')}>Pendentes</button>
-            <button onClick={handleClearCompleted}>#</button>
+            <button onClick={() => setFilter('tudo')}>Todas</button>
+            <button onClick={() => setFilter('completa')}>Concluídas</button>
+            <button onClick={() => setFilter('pendente')}>Pendentes</button>
+            <button onClick={limparConcluida}>🗑️</button>
           </div>
         </div>
       </div>
--- app-inicial/src/pages/index.tsx
+++ app-inicial/src/pages/index.tsx
@@ -13,27 +13,6 @@ function adicionar(){
     <div>
       <TodoList></TodoList>
     </div>
-    
-    //  <body className="
-    // flex justify-center items-center flex-col bg-red-300
-    // h-screen w-screen">
-    //   <p className='font-bold text-3xl'>Pra Fazê!</p>
-    //   <div className= " flex flex-col   h-[32rem] w-[32rem] rounded-xl border-2 border-black border-solid bg-white font-mono font-semibold text-base p-2 gap-2">
-    //         <div className="grid gap-2" >
-    //             <input  className="border-black border-solid border-2 rounded-lg h-10 p-2" type="text" placeholder='Insira uma tarefa' />
-    //             <button className="h-10 rounded-lg border-2 border-black border-solid">Adicionar</button>
-    //         </div>
-    //         <div className="h-screen rounded-lg border-2 border-black border-solid p-2">
-    //             {array.map((item, index)=>())}
-    //         </div>
-    //         <div className="flex flex-row border-black border-solid border-2 rounded-lg h-20 p-2 gap-2 space-x-4">
-    //           <p className="mx-6"><span>#</span> Itens restantes</p>
-    //           <button>Tudo</button>
-    //           <button>Ativas</button>
-    //           <button>Completas</button>
-    //           <button><img src="" alt="" /></button>
-    //         </div>
-    //     </div>
-    // </body>
+  
   )
 }
